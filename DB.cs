c59051fafa919390a3d135e0295b8de958d3a0fa
Thany.Trann <2019000224@student.sit.ac.nz>
2020-07-14 17:07:55 +1200

    DVD Lib

---
+++ DB.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+namespace DVDLib
+{
+    public class DB
+    {
+        const string CSTRING = @"Data Source=C:\Users\sigma\Thany\SQLiteDatabaseBrowserPortable\Databases\DVD_Collection.db;Version=3";
+        //fetch a list of DVDs from the database
+        public static List<DVD> LoadAllDVDs() 
+        {
+            List<DVD> dvds = new List<DVD>();
+           
+            using (SQLiteConnection conn = new SQLiteConnection(CSTRING)) 
+            {
+                conn.Open();
+                string sql_command = string.Format("select * from DVDs");
+                using (SQLiteCommand cmd = new SQLiteCommand(sql_command, conn)) 
+                {
+                    using (SQLiteDataReader rdr = cmd.ExecuteReader()) 
+                    {
+                        while (rdr.Read()) 
+                        {
+                            DVD dvd = new DVD (rdr.GetString(1), 
+                                rdr.GetString(2),
+                                rdr.GetInt32(3),
+                                rdr.GetString(4));
+                            dvd.ID = rdr.GetInt32(0);
+                            dvds.Add(dvd);
+                        }
+                        rdr.Close();
+                    }
+                }
+                conn.Close();
+            }
+
+            return dvds;
+        }
+
+        //Add a DVD into the database
+        public static void AddDVD(DVD dvd) 
+        {
+           
+            using (SQLiteConnection conn = new SQLiteConnection(CSTRING)) 
+            {
+                conn.Open();
+                
+                using (SQLiteCommand cmd = new SQLiteCommand(conn)) 
+                {
+                    cmd.CommandText = string.Format("INSERT INTO DVDs (Title, Director, Year, Language)" +
+                        " VALUES ('{0}', '{1}', {2}, '{3}')", dvd.Title, dvd.Director, dvd.Year, dvd.Language);
+                    cmd.ExecuteNonQuery();
+                }
+
+               conn.Close();
+            }
+        }
+
+        //Update a DVD in the Database (using its id as the key field
+        public static void UpdateDVD(DVD dvd) 
+        {
+           
+            using (SQLiteConnection conn = new SQLiteConnection(CSTRING))
+            {
+                conn.Open();
+                string title = dvd.Title;           
+
+                using (SQLiteCommand cmd = new SQLiteCommand(conn))
+                {
+                    cmd.CommandText = string.Format("UPDATE DVDs SET Title='{0}', Director='{1}', Year={2}, Language='{3}'" +
+                        " WHERE ID={4}", dvd.Title, dvd.Director, dvd.Year, dvd.Language, dvd.ID);
+                    cmd.ExecuteNonQuery();
+                }
+
+                conn.Close();
+            }
+        }
+
+        //Delete a DVD in the database given its id
+        public static void DeleteDVD(int id) 
+        {
+            
+            using (SQLiteConnection conn = new SQLiteConnection(CSTRING))
+            {
+                conn.Open();
+               
+
+                using (SQLiteCommand cmd = new SQLiteCommand(conn))
+                {
+                    cmd.CommandText = string.Format("DELETE FROM DVDs WHERE ID='{0}'", id);
+                    cmd.ExecuteNonQuery();
+                }
+
+                conn.Close();
+            }
+        }
+    }
+}
---
+++ DVD.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DVDLib
+{
+    public class DVD
+    {
+        public int ID { get; set; }
+        public int Year { get; set; }
+        public string Title { get; set; }
+        public string Director { get; set; }
+        public string Language { get; set; }
+
+        public DVD(string title, string director, int year, string language) 
+        {
+           // ID = id;
+            Year = year;
+            Title = title;
+            Director = director;
+            Language = language;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Title: {0} \nDirector: {1} \nLanguage: {2}\nYear: {3} \nID:{4}", Title, Director, Language, Year, ID);
+        }
+    }
+}
